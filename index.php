0dd64b5083b978f22e54793ed82373c06ea8e859
Chris G Johnson <chrisjohnson112@yahoo.com>
2023-10-05 10:04:48 -0500

    init upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>REST PHP Test Page</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+
+
+<table class="table">
+<thead>
+      <tr>
+        <th></th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Rating</th>
+        <th>Image</th>
+      </tr>
+    </thead>
+    <tbody id="table_body">
+      
+     
+    </tbody>
+</table>
+</div>
+<!-- jQuery library -->
+<!-- Latest compiled JavaScript -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,36 @@
+//console.log("Hello world");
+
+//fetch('https://fakestoreapi.com/products').then((data) =>{
+
+//this gets the JSON from the data file included 
+fetch('http://localhost/REST_API/data.json').then((data) =>{
+    //console.log(data);
+    return data.json();///converted to object
+}).then((objectData) => {
+    //console.log(objectData[0].title);
+    let tableData = "";
+    objectData.map((values) => {
+//tableData += shows alll data from API
+tableData+= 
+        `<tr>
+            <td>${values.id}</td>
+            <td>${values.title}</td>
+            <td>${values.description}</td>
+            <td>${values.price}</td>
+            <td>${values.rating.rate}</td>
+            <td><img src="${values.image}" /></td>
+        </tr>`;
+       
+    });
+    document.getElementById("table_body").innerHTML = tableData;
+   
+}).catch((err) => {
+    console.log(err);
+});
+
+
+
+
+
+
+
